5e5035c1d3c4886383c7268a40728e5ae3e6c100
Mohd Imaduddin <82340857+imad-collab@users.noreply.github.com>
2023-05-01 23:46:22 +0530

    Add files via upload

---
+++ JAVA PROGRAMS/Arrays/Arrays 2/src/String Creation .java
@@ -0,0 +1,9 @@
+class demo{
+    public static void main(String args[]){
+        String cars[]={"volvo","hyundai","honda","BMW","Mercedes"};
+        System.out.println("list of cars :- ");
+        for (int i=0;i<cars.length;i++){
+            System.out.println(cars[i]);
+        }
+    }
+}
