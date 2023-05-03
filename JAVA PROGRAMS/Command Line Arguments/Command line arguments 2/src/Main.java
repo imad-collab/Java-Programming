018e86e75ea9e87aa9d780a9c29a62906dd9532b
Mohd Imaduddin <82340857+imad-collab@users.noreply.github.com>
2023-05-03 22:08:26 +0530

    Add files via upload

---
+++ JAVA PROGRAMS/Command Line Arguments/Command line arguments 2/src/Main.java
@@ -0,0 +1,8 @@
+class arguments{
+    public static void main(String args[]){
+        int n= args.length;
+        System.out.println("no of arguments: "+ n);
+        System.out.println("the arguments are: ");
+        for (in)
+    }
+}
